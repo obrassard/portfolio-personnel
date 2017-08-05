70fa3e4ea2d8c8b1e1b2f43b5aaad3bac3d34746
obrassard <brassard.oli@gmail.com>
2017-08-04 21:55:56 -0400

    Condensed 2 JS file in 1

---
+++ js/behavior.js
@@ -0,0 +1,36 @@
+
+//Fadein animation of the header
+$(document).ready(function () {
+    $('header.intro').fadeIn(2000, function(){
+        $('div.fadein').fadeIn(2000);
+    });
+});
+
+
+// jQuery to collapse the navbar on scroll
+function collapseNavbar() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+}
+
+$(window).scroll(collapseNavbar);
+$(document).ready(collapseNavbar);
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $(".navbar-collapse").collapse('hide');
+});
--- js/fade.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function () {
-    $('header.intro').fadeIn(2000, function(){
-        $('div.fadein').fadeIn(2000);
-    });
-});
