71cf18200120ab4b582ee4e0e92e2ebb4ed29285
Olivier Brassard <brassard.oli@gmail.com>
2018-09-09 21:10:39 -0400

    Ajout du landing page au repo github

--- index.php
+++ index.php
@@ -18,6 +18,17 @@ $technologies = GetTechnologies();
 <!DOCTYPE html>
 <html lang="fr">
 <head>
+    
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-125407681-1"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-125407681-1');
+    </script>
+
     <meta charset="utf-8">
     <link rel="icon" type="image/png" href="img/components.png">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -38,8 +49,6 @@ $technologies = GetTechnologies();
 
     <!-- Theme CSS -->
     <link href="css/theme.css" rel="stylesheet">
-
-
 </head>
 
 <body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
@@ -268,16 +277,6 @@ $technologies = GetTechnologies();
     <!-- Theme JavaScript -->
     <script src="js/behavior.js"></script>
     <script src="./js/contactform.js"></script>
-    <script>
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-        ga('create', 'UA-104984331-1', 'auto');
-        ga('send', 'pageview');
-
-    </script>
 </body>
 
 </html>
