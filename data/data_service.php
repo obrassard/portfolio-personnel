248cffbcdbe90fdd20f65bf98cfe174926a79f10
Olivier Brassard <brassard.oli@gmail.com>
2019-06-12 22:48:32 -0400

    Replaced MySQL by MongoDB

---
+++ data/data_service.php
@@ -0,0 +1,66 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/env.php';
+
+
+$mongo = new MongoDB\Client(
+    'mongodb://'.$env['DB_USER'].':'.$env["DB_PASSWORD"].'@'.$env["CLUSTER"]
+);
+
+$db = $mongo->portfolio;
+
+$projects = $db->projects;
+$admins = $db->admins;
+$technos = $db->technologies;
+
+
+function requestLogin($username, $token){
+   
+    global $admins;
+    $user = $admins->findOne(["username"=> $username]);
+
+    if(is_null($user)){
+        return false;
+    }
+
+    if ($user->token == $token){
+        $_SESSION["adminId"] = (string)$user->_id;
+        return true;
+    }
+    return false;
+}
+
+function GetProjects(){
+    global $projects;
+
+    $pdata = $projects->find([], ['sort'=>["_id"=>-1]]);
+    $pdata = iterator_to_array($pdata);
+    return $pdata;
+}
+
+function AddProject($title, $capfr, $capen, $descfr, $descen, $url, $image){
+    global $projects;
+
+    $doc = [
+        "title"=>$title,
+        "image"=>$image,
+        "url"=>$url,
+        "caption" => [
+            "fr"=>$capfr,
+            "en"=>$capen
+        ],
+        "description" => [
+            "fr"=>$descfr,
+            "en"=>$descen
+        ]
+    ];
+
+    $projects->insert($doc);
+}
+
+function GetTechnologies(){
+    global $technos;
+    $techs = $technos->find(['display'=>true]);
+    $techs = iterator_to_array($techs);
+    return $techs;
+}
